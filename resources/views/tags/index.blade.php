e82e339c53f386c09194463e67c1fc01fcb02104
mhmmdva <vallenleyss@gmail.com>
2022-09-22 21:00:19 +0700

    Initial Commit ke-dua

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <div class="d-flex justify-content-center align-items-center tag-category">
-        <h1 class="card-title">
-            <a href="{{ route('tags.create') }}" class="title text-decoration-none"> Tags</a>
 
+        <h1 class="title mt-5 text-decoration-none">
+            <a href="{{ route('tags.index') }}" class="links">Tag</a>
         </h1>
     </div>
 
@@ -17,49 +17,39 @@
         </div>
     @endif
 
-    <div class="d-flex flex-wrap flex-md-nowrap justify-content-center align-items-center pt-3 pb-2 mb-3 ">
-        <div class="card mt-3 col-lg-7 shadow  rounded bg-white">
+    <div class="container">
+        <div class="d-flex justify-content-center align-items-center pt-3 pb-2 mb-3 ">
 
-            <div class="row">
+            <div
+                class=" card mt-3 col-lg-8 shadow  rounded bg-white d-flex flex-wrap flex-lg-wrap justify-content-between justify-content-center align-items-center">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card-body cards-category-name tag-category">
+                            <h3 class="card-title text-center border-bottom border-2 p-1">
+                                All Tags in Websites
+                            </h3>
 
-                <div class="col-lg-12 d-flex align-items-center ">
-
-                    <div class="card-body cards-category-name tag-category">
-                        {{-- <h2 class="card-title py-2 border-bottom">{{ auth()->user()->name }}</h2> --}}
-                        <h3 class="card-title text-center border-bottom border-2 p-1">All Tags in Websites
-                        </h3>
-
-                        @forelse ($tags as $tag)
-                            <a href="{{ route('tags.show', $tag->name) }}"
-                                class="tags btn btn-light card-title mt-3  border rounded text-decoration-none">
-                                #{{ $tag->name }}
-                            </a>
-                        @empty
-                            <p>No tags yet.</p>
-                        @endforelse
-
-                        {{-- <div class="mt-3 d-flex justify-content-between">
-
-                            <button type="button" class="likes btn btn-danger position-relative me-lg-5 p-1 m-0 fs-6">
-                                <i class="bi bi-heart"></i> <i class='bx bx-like align-middle'></i> Likes
-                                <span
-                                    class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-primary">+22
-                                    <span class="visually-hidden">unread messages</span>
-                                </span>
-                            </button>
-
-                            <button type="button" class="btn btn-outline-warning">
-                                <i class="bi bi-bookmark-star"></i>
-                            </button>
-
-
-                        </div> --}}
+                            <div class="col-md-9">
+                                <div
+                                    class="d-flex flex-wrap flex-lg-wrap justify-content-between justify-content-center align-items-center">
+                                    @forelse ($tags as $tag)
+                                        <a href="{{ route('tags.show', $tag->name) }}"\
+                                            class="tags btn btn-light card-title mt-3  border rounded text-decoration-none ">
+                                            #{{ $tag->name }}
+                                        </a>
+                                    @empty
+                                        <p>No tags yet.</p>
+                                    @endforelse
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
-                {{-- {{ $tags->links() }} --}}
+                {{ $tags->links() }}
             </div>
 
         </div>
+    </div>
     </div>
 @endsection
